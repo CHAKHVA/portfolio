0399dda96edb7543a471d3b1bc1665d41be375b1
Aleksandre Chakhvashvili <aleksandre.chakhvashvili24@gmail.com>
2023-08-08 12:20:22 +0400

    projects

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -7,18 +7,6 @@ export default function Projects() {
             id="projects"
             className="w-full h-screen text-center flex flex-col items-center justify-center gap-[3rem] px-12"
         >
-            <div className="text-4xl font-bold">
-                Back-End <span className="text-[#512bd4]">.NET</span> Developer
-                👋
-            </div>
-            <div className="text-lg font-medium">
-                Hi, I&apos;m&nbsp;
-                <span className="text-[#512bd4]">
-                    Aleksandre Chakhvashvili.
-                </span>
-                &nbsp; A passionate for crafting efficient and scalable
-                solutions. Proficient in various languages and frameworks.
-            </div>
             <div>
                 <ul className="flex items-center gap-6 [&>li]:p-2 [&>li]:rounded-full [&>li]:shadow-md [&>li]:cursor-pointer [&>li]:transition [&>li]:duration-300 hover:[&>li]:text-[#512bd4]">
                     <li>
